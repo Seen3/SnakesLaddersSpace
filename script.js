e2467140a199e4a7bb5b517fbeb1f8b18217284f
Seen3 <63313585+Seen3@users.noreply.github.com>
2020-11-19 17:05:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ for (let i=0;i<36;i++)
 {
     let block=document.createElement("div");
     block.className='block';
+    block.id=`block${i}`;
     block.innerHTML=`<p>${i}</p>`
     blocks.push(block);
     board.appendChild(block);
 }
+class Player{
+    constructor()
+    {
+        this.position=0;
+    }
+    moveAhead(n){
+        this.position+=n;
+    }
+    moveTo(n){
+        this.position=n;
+    }
+};
+
 console.log(blocks);
