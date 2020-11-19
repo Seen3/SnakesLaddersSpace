497ef43115904c57f6f13f385d03aefcc91a31a9
Seen3 <63313585+Seen3@users.noreply.github.com>
2020-11-19 17:27:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let board=document.querySelector("#board");
+let dieButton=document.querySelector("#RollDie");
+let turn=document.querySelector("#turns");
+let currentPlayer=1;
 let blocks=[];
 for (let i=0;i<36;i++)
 {
@@ -9,6 +12,10 @@ for (let i=0;i<36;i++)
     blocks.push(block);
     board.appendChild(block);
 }
+function dieRoll(){
+    return Math.floor((Math.random()*6)+1);
+}
+
 class Player{
     constructor()
     {
@@ -16,10 +23,30 @@ class Player{
     }
     moveAhead(n){
         this.position+=n;
+        if(this.position>35)
+        {
+            this.position=35;
+        }
     }
     moveTo(n){
         this.position=n;
     }
 };
 
-console.log(blocks);
+let p1=new Player;
+let p2=new Player;
+dieButton.addEventListener('click',()=>{
+    let dieVal=dieRoll();
+    console.log(dieVal,currentPlayer);
+    if(currentPlayer==1)
+    {
+        currentPlayer=2;
+        p1.moveAhead(dieVal);
+    }
+    else{
+        currentPlayer=1;
+        p2.moveAhead(dieVal);
+    }
+    turn.innerHTML=`<p>Player ${currentPlayer} turn</p>`
+    console.log(`p1=${p1.position}  p2=${p2.position}`);
+})
